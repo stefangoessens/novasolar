5b85090d7b49e8198ffa089205e37351fc10db09
stefangoessens <goessens.stefan@gmail.com>
2025-04-02 14:57:08 -0400

    Update mobile sticky button text to 'GRATIS OFFERTE'
    Change the mobile sticky button text from 'BEREKEN UW VOORDEEL' to 'GRATIS OFFERTE' to better reflect its purpose

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1157,7 +1157,7 @@ const LandingPage = () => {
             onClick={scrollToCalculator}
             className="w-full py-3 px-4 bg-[#FFB366] text-black font-bold rounded-lg shadow-md hover:bg-[#FFA64D] transition-colors flex items-center justify-center"
           >
-            <span>BEREKEN UW VOORDEEL</span>
+            <span>GRATIS OFFERTE</span>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clipRule="evenodd" />
             </svg>
